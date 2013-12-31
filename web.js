3f173dcae4a96e0f03bf28b4057f1753c6884cff
Nathanael Jones <nathanael.jones@gmail.com>
2013-12-30 23:50:05 -0500

    Make everything run on port 80. Make default page display chat.

--- web.js
+++ web.js
@@ -1,13 +1,32 @@
+var express = require('express');
+var app = module.exports = express();
+var server = require('http').createServer(app)
+var io = require("socket.io").listen(server);
+
+
+app.configure(function() {
+    app.locals.pretty = true;
+      app.use(express.bodyParser());
+      app.use(express.methodOverride());
+      app.use(express.static(__dirname + '/public'));
+      app.use('/components', express.static(__dirname + '/components'));
+      app.use('/js', express.static(__dirname + '/js'));
+      app.use('/icons', express.static(__dirname + '/icons'));
+      app.set('views', __dirname + '/views');
+      app.engine('html', require('ejs').renderFile);
+});
+
+app.get('/', function(req, res) {
+  res.render('index.html');
+});
+
+
 var DEBUG = true
 var PORT = 3000
 var INIT_MESSAGES = 5
 
-var http = require('http')
 
-var server = http.createServer()
-server.listen(PORT)
 
-var io = require('socket.io').listen(server)
 io.set ('transports', ['xhr-polling', 'jsonp-polling'])
 
 var messages = new Array()
@@ -44,3 +63,5 @@ io.sockets.on('connection', function(client) {
             console.log("Disconnected: ", client.id)
     })
 })
+
+server.listen(80)
