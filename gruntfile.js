2f49b6dfa56d9e0e1ffa4389c1ac6776018c004d
Nathanael Jones <nathanael.jones@gmail.com>
2014-01-08 21:21:30 -0500

    Add grunt

---
+++ gruntfile.js
@@ -0,0 +1,80 @@
+'use strict';
+
+module.exports = function(grunt) {
+    // Project Configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            js: {
+                files: ['gruntfile.js', 'server.js', 'chat.js', 'public/=**'],
+                tasks: ['jshint'],
+                options: {
+                    livereload: true,
+                },
+            },
+            html: {
+                files: ['public/**'],
+                options: {
+                    livereload: true,
+                },
+            },
+            css: {
+                files: ['public/**'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+        jshint: {
+            all: {
+                src: ['gruntfile.js', 'server.js', 'app/**/*.js',
+                'public/js/**', 'test/**/*.js','!test/coverage/**','!test/busted/**'],
+                options: {
+                    jshintrc: true
+                }
+            }
+        },
+        nodemon: {
+            dev: {
+                options: {
+                    file: 'web.js',
+                    args: [],
+                    ignoredFiles: ['public/**'],
+                    watchedExtensions: ['js'],
+                    nodeArgs: ['--debug'],
+                    delayTime: 1,
+                    env: {
+                        PORT: 80
+                    },
+                    cwd: __dirname
+                }
+            }
+        },
+        concurrent: {
+            tasks: ['nodemon', 'watch'],
+            options: {
+                logConcurrentOutput: true
+            }
+        },
+        env: {
+            test: {
+                NODE_ENV: 'test'
+            }
+        }
+    });
+
+    //Load NPM tasks 
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-nodemon');
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-env');
+
+    //Making grunt default to force in order not to break the project.
+    grunt.option('force', true);
+
+    //Default task(s).
+    grunt.registerTask('default', ['jshint', 'concurrent']);
+
+    //Test task.
+};
